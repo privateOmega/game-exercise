5ae5842b2751daef9117c110806a095d5a4ff1c4
privateOmega <privateOmega@protonmail.com>
2020-02-29 15:22:21 +0530

    feat(server): added route handlers for register and login routes

--- server/routes/main.route.js
+++ server/routes/main.route.js
@@ -1,14 +1,59 @@
 const express = require('express');
 
 const asyncMiddleware = require('../utils/async-middleware');
+const userModel = require('../models/user.model');
 
 const mainRouter = express.Router();
 
 mainRouter.get(
   '/status',
   asyncMiddleware((req, res, next) => {
-    res.status(200);
-    res.json({ status: 'ok' });
+    res.status(200).json({ status: 'ok' });
+  }),
+);
+
+mainRouter.post(
+  '/register',
+  asyncMiddleware(async (req, res, next) => {
+    const { firstName, lastName, email, password } = req.body;
+    const userInstance = await userModel.create({
+      firstName,
+      lastName,
+      email,
+      password,
+    });
+    const token = userInstance.generateAuthToken();
+    res.status(200).json({
+      status: 'ok',
+      user: {
+        email: userInstance.email,
+        name: userInstance.name,
+        highScore: userInstance.highScore,
+        token,
+      },
+    });
+  }),
+);
+
+mainRouter.post(
+  '/login',
+  asyncMiddleware(async (req, res, next) => {
+    const { email, password } = req.body;
+    const userInstance = await userModel
+      .findByCredentials(email, password)
+      .catch(error => {
+        res.status(401).json({ message: 'unauthenticated' });
+      });
+    const token = userInstance.generateAuthToken();
+    res.status(200).json({
+      status: 'ok',
+      user: {
+        email: userInstance.email,
+        name: userInstance.name,
+        highScore: userInstance.highScore,
+        token,
+      },
+    });
   }),
 );
 
