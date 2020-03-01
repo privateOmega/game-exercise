f5af962c6b2772261d7dd366aa2a97d203dd9b27
privateOmega <privateOmega@protonmail.com>
2020-03-01 21:25:00 +0530

    feat(server): added validators for user schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -11,19 +11,24 @@ const userSchema = new Schema(
     firstName: {
       type: String,
       required: true,
+      trim: true,
     },
     lastName: {
       type: String,
       required: true,
+      trim: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
+      trim: true,
+      lowercase: true,
     },
     password: {
       type: String,
       required: true,
+      minlength: 6,
     },
     highScore: {
       type: Number,
